87127f3cb68d6ae08a0b17ccf95cd59a4b7e8e70
Riyadh Al Nur <riyadhalnur@verticalaxisbd.com>
2020-05-24 23:54:40 +0800

    Remove old  and unecessary files

--- test/setup/node.js
+++
@@ -1,9 +0,0 @@
-let chai = require('chai');
-
-global.chai = chai;
-global.sinon = require('sinon');
-global.chai.use(require('sinon-chai'));
-global.should = chai.should();
-
-require('babel-core/register');
-require('./setup')();
--- types/index.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'node-base64-image' {
-    type Callback<T> = (err: any, x?: T) => void
-
-    function encode(url: string, options: { string?: boolean, local?: boolean }, callback: Callback<any>): void
-
-    function decode(imageBuffer: any, options: { filename?: string }, callback: Callback<any>): void
-}
